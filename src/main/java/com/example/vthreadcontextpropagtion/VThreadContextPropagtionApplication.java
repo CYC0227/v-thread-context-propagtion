19a4135e11b47f2fc3032499c54115283e72bcc4
yoonchan <cyc0227@gachon.ac.kr>
2025-04-05 13:29:48 +0900

    init

---
+++ src/main/java/com/example/vthreadcontextpropagtion/VThreadContextPropagtionApplication.java
@@ -0,0 +1,13 @@
+package com.example.vthreadcontextpropagtion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VThreadContextPropagtionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VThreadContextPropagtionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vthreadcontextpropagtion/VThreadContextPropagtionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vthreadcontextpropagtion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VThreadContextPropagtionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
